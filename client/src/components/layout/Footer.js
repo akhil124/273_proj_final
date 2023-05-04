cd14d0ccbc465a092e5c58610fbc89274220790f
Akhil <akhil30.ar6@gmail.com>
2023-05-04 13:36:06 -0700

    proj

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer grey darken-4">
+      <div className="container white-text">
+        <div className="left">
+          {/* <h6>Copyright &copy; {new Date().getFullYear()}</h6> */}
+        </div>
+        <div className="right">
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
